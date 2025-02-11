71961bf929e3618a13e07e58242e32ce8857c72f
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-02-11 16:26:22 +0530

    updated

--- src/app/admin/getBlog/page.jsx
+++ src/app/admin/getBlog/page.jsx
@@ -1,17 +1,7 @@
-"use client";
 import GetBlog from "@/components/Admin/pages/GetBlogs";
-import React, { useEffect, useState } from "react";
 
 const page = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return null;
-  }
+ 
   return (
     <div>
       <GetBlog />
